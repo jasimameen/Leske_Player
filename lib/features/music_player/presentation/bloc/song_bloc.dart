c3a031034d961678e86cbb597d7c24f7720bbc0e
Jasim Ameen <89957373+jasimameen@users.noreply.github.com>
2022-11-01 14:44:12 +0530

    change import bloc to flutter_bloc

--- lib/features/music_player/presentation/bloc/song_bloc.dart
+++ lib/features/music_player/presentation/bloc/song_bloc.dart
@@ -1,12 +1,9 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:bloc/bloc.dart';
-import 'package:dartz/dartz.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 import 'package:music_player/features/music_player/domain/entities/song.dart';
 import 'package:music_player/features/music_player/domain/repositories/song_repository.dart';
 
-import '../../../../core/error/failure.dart';
 
 part 'song_bloc.freezed.dart';
 part 'song_event.dart';
